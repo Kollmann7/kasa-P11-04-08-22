365bae486cf0eb4631e2a54cb7a1ad00cb1e740c
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-08-04 17:47:26 +0200

    change files names

---
+++ src/Gallery.js
@@ -0,0 +1,15 @@
+import Banner from './assets/banner.png';
+import './styles/Gallery.css';
+
+function Gallery() {
+    return(
+        <div className='gallery'>
+            
+        </div>
+
+    )
+        
+    
+}
+
+export default Gallery
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
+import Header from './Header';
 import reportWebVitals from './reportWebVitals';
+import Gallery from './Gallery';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const header = ReactDOM.createRoot(document.getElementById('header'));
+header.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>
 );
-
+const gallery = ReactDOM.createRoot(document.getElementById('gallery'))
+gallery.render(
+  <React.StrictMode>
+    <Gallery />
+  </React.StrictMode>
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
