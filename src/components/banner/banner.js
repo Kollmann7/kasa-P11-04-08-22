9db31d2ecc402a1df378e4acd84cd68e578425bc
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-08-11 16:06:59 +0200

    add home banner

---
+++ src/components/banner/banner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './banner.css'
+
+export default class Banner extends React.Component{
+    render(){
+        return(
+            <div className='banner-wrapper'><div className="banner"><h2>Chez vous, partout ailleur</h2></div></div>
+        )
+    }
+}
