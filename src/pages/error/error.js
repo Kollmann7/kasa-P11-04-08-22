118fdc9ea6c6faf4b663b21d29e7f4924d774a7c
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-08-11 18:31:51 +0200

    add prettier

--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -3,23 +3,21 @@ import { Link } from 'react-router-dom'
 import HeaderLayout from '../../layout/headerLayout'
 import './error.css'
 
-
 export default class Error extends React.Component {
-    render() {
-      return (
-        <HeaderLayout>
-          <main className="error">
-            <div>
-              <h2>404</h2>
-              <span>
-                Oups! La page que
-                <br /> vous demandez n'existe pas.
-              </span>
-            </div>
-            <Link to="/">Retourner sur la page d'accueil</Link>
-          </main>
-        </HeaderLayout>
-      )
-    }
+  render() {
+    return (
+      <HeaderLayout>
+        <main className="error">
+          <div>
+            <h2>404</h2>
+            <span>
+              Oups! La page que
+              <br /> vous demandez n'existe pas.
+            </span>
+          </div>
+          <Link to="/">Retourner sur la page d'accueil</Link>
+        </main>
+      </HeaderLayout>
+    )
   }
-  
+}
