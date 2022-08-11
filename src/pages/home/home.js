3c79286d6edcbd6f9b771aadd1b6f8143f0f9fb1
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-08-11 17:58:20 +0200

    add data path

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import MainLayout from '../../layout/mainLayout'
 import Banner from '../../components/banner/banner'
-
+import LocationList from '../../components/locationList/locationList'
 
 export default class Home extends React.Component {
-    render(){
-        return(
-            <MainLayout>
-                <main className="home">
-                    <Banner />
-                </main>
-            </MainLayout>
-        )
+  constructor(props) {
+    super(props)
+    this.title = <h2>Chez vous, partout et ailleurs</h2>
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: [],
     }
+
+  }
+  componentDidMount() {
+    fetch('../data/data.json')
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result)
+        // eslint-disable-next-line no-unused-expressions
+        this.setState({
+          isLoaded: true,
+          data: result
+        }),
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            })
+          }
+      })
+  }
+  
+  render() {
+    return (
+      <MainLayout>
+        <main className="home">
+          <Banner title={this.title} />
+          {/* <LocationList data={this.state.data} /> */}
+        </main>
+      </MainLayout>
+    )
+  }
 }
