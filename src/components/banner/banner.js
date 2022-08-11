7734808d8d61ebb7c4d7a6426a0dae4474defcac
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-08-11 17:57:20 +0200

    add new banner

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './banner.css'
 
-export default class Banner extends React.Component{
-    render(){
-        return(
-            <div className='banner-wrapper'><div className="banner"><h2>Chez vous, partout ailleur</h2></div></div>
-        )
-    }
+export default class Banner extends React.Component {
+  render() {
+    return (
+      <div className="banner-wrapper">
+        <div className="banner">{this.props.title}</div>
+      </div>
+    )
+  }
 }
