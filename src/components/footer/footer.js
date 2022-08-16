cfc7953ca665abb6370e9c7ec44193bb960e8740
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-08-17 01:46:08 +0200

    add logos

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from "../../assets/logo-footer.png";
 import './footer.css'
 
 function Footer() {
   return (
     <footer>
+      <img src={logo} alt="logo" />
       <strong>© 2021 Kasa. All rights reserved</strong>
     </footer>
   )
