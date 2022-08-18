5546aabec4fa6de182045cd0a327e6af803cc029
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-08-18 10:17:19 +0200

    add catch error

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,31 +1,31 @@
-import React,{ useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import MainLayout from '../../layout/mainLayout'
 import Banner from '../../components/banner/banner'
 import LocationList from '../../components/locationList/locationList'
 import './home.css'
 
-function Home () {
-  const [setError] = useState(null)
+function Home() {
+  const [, setError] = useState(null)
   const [data, setData] = useState([])
 
   useEffect(() => {
     fetch('./data/data.json')
-    .then((res) => res.json())
+      .then((res) => res.json())
       .then((result) => {
         setData(result)
-      },(error) => {
+      })
+      .catch((error) => {
         setError(error)
-      }
-      )
-    }, [])
-    return (
-      <MainLayout>
-        <main className="home">
-          <Banner title='Chez vous, partout et ailleurs' />
-          
-          <LocationList data={data} />
-        </main>
-      </MainLayout>
-    )
+      })
+  }, [])
+
+  return (
+    <MainLayout>
+      <main className="home">
+        <Banner title="Chez vous, partout et ailleurs" />
+        <LocationList data={data} />
+      </main>
+    </MainLayout>
+  )
 }
 export default Home
